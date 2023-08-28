e1eed84fc859f95d5ec6d9a8bb468c0cafebb71e
leesochay <sochaylee@gmail.com>
2023-08-28 11:50:57 -0400

    separating components for Header, Footer, and Project

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import leePhoto from './LeeSochay.jpg';
+
+function Header() {
+    return (
+<header className="header">
+    <figure className="headshot">
+        <img className="leephoto" src={ leePhoto } alt="Lee Sochay Headshot"></img>
+    <figcaption className="caption">Lee Sochay - Full Stack Developer</figcaption>
+    </figure>
+	<nav className="nav">
+	<ul className="nav-list">
+        <li><a className="nav-link" href="/">Home</a></li>
+        <li><a className="nav-link" href="#about">About</a></li>
+        <li><a className="nav-link" href="#projects">Projects</a></li>
+        <li><a className="nav-link" href="#contact">Contact</a></li>
+	</ul>
+    </nav>
+</header>
+	)}
+
+export default Header
